ce2d47e6464d185a5a38a7683e97583efb150a13
Kaushik Mishra <kmishra27.km@gmail.com>
2020-04-03 21:21:41 +0530

    trial

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -44,7 +44,6 @@ class MemeGenerator extends Component {
                 <form className="meme-form" onSubmit={this.handleSubmit}>
                     <input type="text" name="topText" placeholder="Top Text" value={this.state.topText} onChange={this.handleChange}/> 
                     <input type="text" name="bottomText" placeholder="Bottom Text" value={this.state.bottomText} onChange={this.handleChange}/> 
-                
                     <button>New</button>
                 </form>
                 <div className="meme">
